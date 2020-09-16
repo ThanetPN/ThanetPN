1ce29fc6f2d78b30b9a4ce61618c1524c5cccf3b
DESKTOP-S3P6D7F\Type <Thanetpnn@gmailcom>
2020-09-16 13:37:36 +0700

    Update example.js

--- js/example.js
+++ js/example.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+let name = 'type'
+let test = 'hello'
+
+name = `${test} B` 
+let arr= ['apple','banana','kiwi']
+
+let person = {
+    name: 'Type',
+    age: 21,
+    gender: {
+        name: 'male',
+        age: 22
+    },
+    arr
+}
+console.log(person)
