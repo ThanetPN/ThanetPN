37407daea9ae3177e8e1aa63b65c1dee4fe366e2
DESKTOP-S3P6D7F\Type <Thanetpnn@gmailcom>
2020-09-16 15:13:19 +0700

    Create quiz2.js

---
+++ js/quiz2.js
@@ -0,0 +1,24 @@
+1
+for( let i=1; i<=20; i++){
+    console.log(i)
+}
+2
+for(let i =1; i<=20; i++){
+    if(i%2 ===0){
+        console.log(i)
+    }
+}
+3
+let arr = [-1,2,3,56,6,-93,0,4]
+let a = []
+for (let i = 0; i < arr.length; i++) {
+    a.push(arr[i]*2)
+ }
+console.log(a)
+4
+const number = [10,13,44,55,89,0,12,1]
+for(let i=0;i < number.length; i++){
+   if(i%2 ===0){
+    console.log(i)
+   } 
+}
